22b9ebf234e5eaf2f4f7a4287bcce24b7cd1dade
Jonathan Guerrera <jrrera@gmail.com>
2020-01-26 19:03:14 -0800

    Use source field from json response

--- src/AudioStatus.js
+++ src/AudioStatus.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const JSON_SOURCE = "http://34.68.79.148:8000/status-json.xsl";
+const JSON_SOURCE = "https://stream.djmicrobeat.com:8443/status-json.xsl";
 
 function AudioStatus() {
   const [isStreaming, setStreaming] = useState(false);
@@ -12,8 +12,7 @@ function AudioStatus() {
     .then(json => {
       if (json.icestats && json.icestats.source) {
         setStreaming(true);
-        // TODO: Fix the source.listenurl field in server config
-        setStreamUrl("http://34.68.79.148:8000/techhouse");
+        setStreamUrl(json.icestats.source);
       }
     });
 
